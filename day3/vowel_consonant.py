e7920e0ee28687b464cc18b57b6cd8c0b96b7354
Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-10-04 18:36:49 -0400

    edit last exo

--- day3/vowel_consonant.py
+++ day3/vowel_consonant.py
@@ -9,7 +9,10 @@ small_letters = "abcdefghijklmnopqrstuvwxyz"
 count_small_letters = 0
 
 
-consonants = "bcdefghjklmnpqrstvwxz"
+count_consonants = 0
+count_vowels = 0
+
+consonants = "bcdfghjklmnpqrstvwxz"
 vowels = "aeiouy"
 
 consonants_tab = []
@@ -28,15 +31,21 @@ for x in small_letters:
 for x in consonants:
     for y in word.lower():
         if x == y:
+            count_consonants += 1
             consonants_tab.append(y)
+
 
 for x in vowels:
     for y in word.lower():
         if x == y:
+            count_vowels += 1
             vowels_tab.append(y)
 
 print(f"Cap letters : {count_cap_letters} ")
 print(f"Small letters : {count_small_letters}")
 
+print(f"Number of consonants : {count_consonants} ")
 print(f"consonants : {consonants_tab}")
+
+print(f"Number of vowels : {count_vowels}")
 print(f"vowels : {vowels_tab} ")
