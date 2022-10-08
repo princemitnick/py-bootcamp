7d70f857443dbb46ee232f0c69f1a33ccc881d1a
Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-10-08 14:45:47 -0400

    Hangman game

--- day7/hangman1.py
+++ day7/hangman1.py
@@ -1,24 +1,50 @@
 import random
+import hangman_words
+import hangman_art
 
-words = ["romeo", "mouse", "caviar", "keyboard", "cahier"]
-# word = words[random.randint(0,len(words)-1)]
-word = 'caviviara'
-word_tab = list(word)
-print(word_tab)
+print(hangman_art.logo)
+
+stages = hangman_art.stages
+
+words = hangman_words.word_list
+word = words[random.randint(0, len(words) - 1)]
+# word =
+
 blanks = ["_"] * len(word)
 
-letter = "v"
+
+def show_blanks():
+    for x in blanks:
+        print(x, end="")
 
 
-while blanks.__contains__("_"):
+attempt = len(word) + 2
+
+show_blanks()
+
+count_stages = len(stages)
+
+
+# if "_" not in blanks
+while blanks.__contains__("_") and count_stages != 0:
     count = 0
-    letter = input("letter : ")
-    while count != len(word):
-        if word[count] == letter:
-            blanks[count] = letter
-        count += 1
+    letter = input("\nletter : ").lower()
+    if len(letter) == 0 or len(letter) > 1:
+        print("Only 1 letter")
+    elif letter in word:
+        while count != len(word):
+            if word[count] == letter:
+                blanks[count] = letter
+            count += 1
+    else:
+        print("Failed, retry!")
+        print(stages[count_stages - 1])
+        count_stages -= 1
 
-    print(blanks)
+    show_blanks()
+    attempt -= 1
 
-print("Done.")
-print(blanks)
+if blanks.__contains__("_"):
+    print("\nYou lose.")
+else:
+    print("\nWell Done. Bravo!")
---
+++ day7/hangman2.py
@@ -0,0 +1,13 @@
+import  random
+
+word_list = ["ardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+
+guess = input("Guest a letter: ").lower()
+
+display = []
+
+display += "b"
+
+print(display)
