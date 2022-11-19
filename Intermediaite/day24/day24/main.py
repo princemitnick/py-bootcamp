f703605e417031bf412f4e952c5e3d16b17b3403
Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-11-19 09:20:45 -0500

    Starting game (eat the turtle)

---
+++ Intermediaite/day24/day24/main.py
@@ -0,0 +1,59 @@
+'''
+Author :
+Jean Baptiste Prince STanley Lovensky J.
+Date : 09/11/2022
+'''
+
+
+from turtle import Screen
+import time
+
+from food import Food
+from snake import Snake
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.update()
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        scoreboard.increase_score()
+        snake.extend()
+        food.refresh()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_is_on = False
+
+    # Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.update()
+screen.exitonclick()
---
+++ Intermediaite/project/game1/README.md
@@ -0,0 +1,9 @@
+Create a game where you have a turtle (black) who's going to eat only blue turtle.
+
+# Steps
+
+## Create Screen
+   * Setup Screen (600 x 600)
+   * bgcolor = white
+   * title = eat the turtle
+
